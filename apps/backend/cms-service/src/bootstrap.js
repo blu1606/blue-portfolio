a8fe863b178b9f187fb2a1b8cd2338b077b24c30
HoangBluecodedao <dongthanhquandtq@gmail.com>
2025-08-24 23:15:16 +0700

    refactor(cms-service): setup dependency injection for cms service

---
+++ apps/backend/cms-service/src/bootstrap.js
@@ -0,0 +1,35 @@
+// src/bootstrap.js
+const { Container } = require('./container');
+const supabase = require('./db/initSupabase');
+
+// Import factory functions
+const { createPostRepository } = require('./repositories/postRepository');
+const { createCreatePostUseCase } = require('./usecases/createPost');
+const { createGetPostUseCase } = require('./usecases/getPost');
+
+const setupContainer = () => {
+  const container = new Container();
+
+  // External dependencies
+  container.register('supabase', supabase, { singleton: true });
+
+  // Repository layer
+  container.register('postRepository', (container) => {
+    return createPostRepository(container.get('supabase'));
+  }, { singleton: true });
+
+  // Use case layer
+  container.register('createPostUseCase', (container) => {
+    return createCreatePostUseCase(container.get('postRepository'));
+  });
+
+  container.register('getPostUseCase', (container) => {
+    return createGetPostUseCase(container.get('postRepository'));
+  });
+  
+  // TODO: Register other use cases here
+  
+  return container;
+};
+
+module.exports = { setupContainer };
