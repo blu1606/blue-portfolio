1a1ecd01fbdbf894053d17c794315d43d1d56b9b
HoangBluecodedao <dongthanhquandtq@gmail.com>
2025-08-24 19:40:00 +0700

    chore(cms-service): initial project structure and dependencies

---
+++ apps/backend/cms-service/src/repositories/postRepository.js
@@ -0,0 +1,28 @@
+
+const createPost = (postData) => {
+
+}
+
+const getPostBySlug = (slug) => {
+
+}
+
+const getAllPosts = () => {
+
+}
+
+const updatePost = (postId, updateData) => {
+
+}
+
+const deletePost = (postId) => {
+
+}
+
+module.exports = {
+    createPost,
+    getPostBySlug,
+    getAllPosts,
+    updatePost,
+    deletePost
+}
