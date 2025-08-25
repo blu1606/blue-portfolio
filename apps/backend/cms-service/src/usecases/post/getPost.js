4c990508e0fb4f9ca42ebc54fee75b3f7f64616c
HoangBluecodedao <dongthanhquandtq@gmail.com>
2025-08-25 14:15:17 +0700

    refactor: use getWithStale for getPost usecase

--- apps/backend/cms-service/src/usecases/post/getPost.js
+++ apps/backend/cms-service/src/usecases/post/getPost.js
@@ -1,19 +1,16 @@
 // src/usecases/getPost.js
 const { BadRequestError, NotFoundError } = require('common/core/error.response');
 
-const createGetPostUseCase = (postRepository) => {
+const createGetPostUseCase = (postRepository, cacheService) => {
     return async (slug) => {
         if (!slug) throw new BadRequestError('Post slug is required.');
 
         // search cache before going to db
         const cacheKey = `post:slug:${slug}`;
-        const cachedPost = await cacheService.get(cacheKey);
-        if (cachedPost) {
-            return { post: cachedPost };
-        }
 
-
-        const post = await postRepository.findBySlug(slug);
+        const post = await cacheService.getWithStale(cacheKey, async () => {
+            return postRepository.findBySlug(slug);
+        });
 
         if (!post) throw new NotFoundError('Post not found.');
 
